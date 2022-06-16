462cfc5432c79d54cd7cea0f7cdee6279a92e0fe
livefuture009 <36209712+livefuture009@users.noreply.github.com>
2022-06-16 20:50:10 +0800

    Replace admin email

--- config/sendgrid.config.js
+++ config/sendgrid.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     API_KEY:
         "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",
-    // ADMIN_EMAIL: "billion102@outlook.com",
-    ADMIN_EMAIL: "rocksiri2017@gmail.com",
+    ADMIN_EMAIL: "billion102@outlook.com",
+    // ADMIN_EMAIL: "rocksiri2017@gmail.com",
     SENDER: "sprecourt@us-hc.com",
 };
